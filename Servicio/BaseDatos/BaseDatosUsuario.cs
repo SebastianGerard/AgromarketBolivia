198ba777327b90516102d7ceb016448c6aed3081
Sebastian Gerard <ger.sal.jo@gmail.com>
2013-01-15 18:13:54 -0400

    Clases con la base de facebook

--- Servicio/BaseDatos/BaseDatosUsuario.cs
+++ Servicio/BaseDatos/BaseDatosUsuario.cs
@@ -9,36 +9,22 @@ namespace BaseDatos
 {
     public static class BaseDatosUsuario
     {
-        static NpgsqlConnection conexion = new NpgsqlConnection("Server = 127.0.0.1 ; Port=5432; User Id = postgres; Password = Gerard66. ; Database =Agromarket");
-
-            
-        public static void abrirConexion()
-        {
-           if(conexion.State == System.Data.ConnectionState.Closed)
-                conexion.Open();
-        }
-        public static void cerrarConexion()
-        {
-           
-
-            if (conexion.State == System.Data.ConnectionState.Open)
-                conexion.Close();
-        }
+       
         public static ModeloUsuario ObtenerUsuario(string nombreUsuario)
         {
             try
             {
                 ModeloUsuario usuario = new ModeloUsuario();
-                NpgsqlCommand cmd = new NpgsqlCommand("Select * from usuario where nombreusuario = @nombre", conexion);
-                cmd.Parameters.Add("nombre", nombreUsuario);
-                abrirConexion();
+                NpgsqlCommand cmd = new NpgsqlCommand("Select * from usuario where nombreusuario = @nombre", Conexion.conexion);
+                cmd.Parameters.Add("nombre",nombreUsuario);
+                Conexion.abrirConexion();
                 NpgsqlDataReader reader = cmd.ExecuteReader();
                 if(reader.Read())
                 {
                     usuario.nombreUsuario = reader["nombreusuario"].ToString();
                     usuario.contrasena = reader["contrasena"].ToString();
                 }
-                cerrarConexion();
+                Conexion.cerrarConexion();
                 return usuario;
             }
             catch (Exception ex)
---
+++ Servicio/BaseDatos/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql;
+
+namespace BaseDatos
+{
+    public static class Conexion
+    {
+        public static NpgsqlConnection conexion = new NpgsqlConnection("Server = 127.0.0.1 ; Port=5432; User Id = postgres; Password = Gerard66. ; Database =Agromarket");
+
+
+        public static void abrirConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+        }
+        public static void cerrarConexion()
+        {
+
+
+            if (conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+}
